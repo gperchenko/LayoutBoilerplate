89dd7a94cc390eb215e0f267047c9ed952640fdf
Genady Perchenko <gperchenko@gmail.com>
2018-02-22 12:44:46 -0500

    add 2 new main  components

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
+import { TitleComponent } from './components/title/title.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   imports: [],
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    NavComponent
   ]
 })
 export class MainModule { }
