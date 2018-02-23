2965c99c29477052ac7980f17d52553b051ab6e7
Genady Perchenko <gperchenko@gmail.com>
2018-02-22 21:49:28 -0500

    added new modules

---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContactListComponent]
+})
+export class ContactModule { }
