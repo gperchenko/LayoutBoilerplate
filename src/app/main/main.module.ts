f7990af8568f293d72e82d46d5b926e967205667
Genady Perchenko <gperchenko@gmail.com>
2018-02-23 12:32:46 -0500

    add basick routing to the mains module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { TitleComponent } from './components/title/title.component';
 import { NavComponent } from './components/nav/nav.component';
 
+import { ContactModule } from '../contact/contact.module';
+import { ContactListComponent } from '../contact/components/contact-list/contact-list.component';
+
+import { ItemModule } from '../item/item.module';
+import { ItemListComponent } from '../item/components/item-list/item-list.component';
+
+const routes: Routes = [
+  { path: 'contacts', component: ContactListComponent },
+  { path: 'items', component: ItemListComponent },
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' }
+];
+
 @NgModule({
-  imports: [],
+  imports: [
+    ContactModule,
+    ItemModule,
+    RouterModule.forRoot(routes)
+  ],
   declarations: [
     AppComponent,
     TitleComponent,
